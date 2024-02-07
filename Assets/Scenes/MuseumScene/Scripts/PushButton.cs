9fdd6f33dc5598924b6f49f0537ad64133ff831c
Jean <cogito.jm@outlook.fr>
2024-02-07 15:40:45 +0100

    button working + push to exhibition scene

--- Assets/Scenes/MuseumScene/Scripts/PushButton.cs
+++ Assets/Scenes/MuseumScene/Scripts/PushButton.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
 
 public class PushButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string animationName;
+    private XRBaseInteractable interactable;
+
     void Start()
     {
-        
+        interactable = GetComponent<XRBaseInteractable>();
+        interactable.selectEntered.AddListener(OnButtonPressed);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("LeftController") || other.gameObject.CompareTag("RightController"))
+        {
+            // Enregistre le nom de l'animation
+            PlayerPrefs.SetString("animationName", animationName);
+
+            // Charge la nouvelle scène
+            SceneManager.LoadScene("ExhibitionScene");
+        }
+    }
+           
+
+    void OnButtonPressed(SelectEnterEventArgs args)
+    {
+        // Enregistre le nom de l'animation
+        PlayerPrefs.SetString("animationName", animationName);
+        Debug.Log("yollo");
+        // Charge la nouvelle scène
+        SceneManager.LoadScene("ExhibitionScene");
     }
 }
